88f77bc370943c4a088737e042919527cdd7153f
Yasmine <es.yasmin.haytham2023@alexu.edu.eg>
2025-12-21 20:44:31 +0200

    Update Catalogue.java

--- Sudoku/src/main/java/com/mycompany/sudoku/Catalogue.java
+++ Sudoku/src/main/java/com/mycompany/sudoku/Catalogue.java
@@ -1,21 +1,47 @@
 package com.mycompany.sudoku;
 
 import java.nio.file.Path;
+import java.util.List;
 
 public class Catalogue {
     private Path baseDir;
+    private boolean unfinishedGame;
+    private boolean easyMediumHard;
 
     public Catalogue() {
         this.baseDir = Path.of("games");
-    }
-    public Catalogue(Path baseDir) {
-        this.baseDir = baseDir;
+        this.unfinishedGame = false;
+        this.easyMediumHard = false;
     }
 
     public void setPath (String newBaseDir) {
         this.baseDir = Path.of(newBaseDir);
     }
-    
+
+    public boolean[] checkGames(){
+        FileManager storageFileManager = new FileManager();
+        List<Path> unfinishedGames = storageFileManager.listFiles(baseDir.resolve("incomplete"));
+       List<Path> easyGames = storageFileManager.listFiles(baseDir.resolve("easy"));
+         List<Path> mediumGames = storageFileManager.listFiles(baseDir.resolve("medium"));
+          List<Path> hardGames = storageFileManager.listFiles(baseDir.resolve("hard"));
+        if (unfinishedGames != null && !unfinishedGames.isEmpty()) {
+            this.unfinishedGame = true;
+        } else {
+            this.unfinishedGame = false;
+        }
+        if ((easyGames != null && !easyGames.isEmpty()) &&
+            (mediumGames != null && !mediumGames.isEmpty()) &&
+            (hardGames != null && !hardGames.isEmpty())) {
+            this.easyMediumHard = true;
+        } else {
+            this.easyMediumHard = false;
+        }
+        return new boolean[]{unfinishedGame, easyMediumHard};
+    }
+   
+
+
+
 
 
 
